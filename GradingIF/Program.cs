481845fe2abd3e3866166aba632ba4313ac60c91
Janis Griezans <janis.griezans@etsnord.ee>
2022-02-07 16:11:14 +0200

    add new projects

---
+++ GradingIF/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GradingIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai
+            //kui kasutaja sai 'A', konsool kuvab "Suurepärane!";
+            //kui tulemus on 'B', konsool kuvab "Väga hea!";
+            //kui tulemus on 'C', konsool kuvab "Hea!";
+            //kui tulemus on 'D', konsool kuvab "Rahuldav";
+            //kui tulemus on 'E', konsool kuvab "Kasin";
+            //kui tulemus on 'F', konsool kuvab "Puudulik";
+            //kui kasutaja sisestab midagi muud, siis konsool kuvab "vale väärtus"
+
+            //kasutame IF
+
+            Console.WriteLine("Sisesta oma eksami tulemus:");
+            //char - character
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+
+            if(userResult == 'A')
+            {
+                Console.WriteLine("Suurepärane!");
+            }
+            
+            else if (userResult == 'B') 
+            {
+                Console.WriteLine("Väga hea!");
+            }
+            else if (userResult == 'C') 
+            {
+                Console.WriteLine("Hea!");
+            }
+            else if (userResult == 'D') 
+            {
+                Console.WriteLine("Rahuldav!");
+            }
+            else if (userResult == 'E') 
+            {
+                Console.WriteLine("Kasin!");
+            }
+            else if (userResult == 'F') 
+            {
+                Console.WriteLine("Puudlik, peate töö uuesti sooritama!");
+            }
+            else
+            {
+                Console.WriteLine("Sisestatud väärtus on vale!");
+            }
+
+            Console.WriteLine("Kena päeva jätku teile!");
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai
+            //kui kasutaja sai 'A', konsool kuvab "Suurepärane!";
+            //kui tulemus on 'B', konsool kuvab "Väga hea!";
+            //kui tulemus on 'C', konsool kuvab "Hea!";
+            //kui tulemus on 'D', konsool kuvab "Rahuldav";
+            //kui tulemus on 'E', konsool kuvab "Kasin";
+            //kui tulemus on 'F', konsool kuvab "Puudulik";
+            //kui kasutaja sisestab midagi muud, siis konsool kuvab "vale väärtus"
+
+            //kasutame switchi
+
+            Console.WriteLine("Sisesta oma eksami tulemus:");
+            //char - character
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (userResult)
+            {
+                case 'A':
+                    Console.WriteLine("Suurepärane!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Väga hea");
+                    break;
+                case 'C':
+                    Console.WriteLine("Hea!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Rahuldav!");
+                    break;
+                case 'E':
+                    Console.WriteLine("Kasin!");
+                    break;
+                case 'F':
+                    Console.WriteLine("Puudulik, töö tuleb uuesti sooritada!");
+                    break;
+                default:
+                    Console.WriteLine("Vale väärtus");
+                    break;
+            }
+            Console.WriteLine("Kena päeva jätku!");
+
+
+
+        }
+    }
+}
